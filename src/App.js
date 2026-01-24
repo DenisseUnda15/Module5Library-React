8c9ab02ccace538696243a4598426457c2808bf6
Denisse Unda <chiqui.samy7@gmail.com>
2026-01-24 11:58:25 -0500

    checkout button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -6,21 +6,28 @@ import Home from './pages/Home';
 import Books from './pages/Books';
 import { books } from "./data";
 import BookInfo from './pages/BookInfo';
-
-
+import Cart from './pages/Cart';
 
 function App() {
+  const [cart,setCart] = useState([]);
+  
+  function addToCart (book) {
+    setCart([...cart, book])
+  }
+
+  useEffect (() => {
+    console.log(cart)
+  }, [cart])
+
+
   return (
     <Router>
      <div className="App">
        <Nav />
-
-       <Switch>
        <Route path="/" exact component={Home} />
-       <Route path="/books" render={() => <Books books={books} />} />
-       <Route path="/books/1" render={()=> <BookInfo books={books} />} />
-       </Switch>
-
+       <Route path="/books" exact render={() => <Books books={books} />} />
+       <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart}/>} />
+       <Route path="/cart" render={()=> <Cart books={books} cart={cart}/>} />
        <Footer />
     </div>
     </Router>
--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faShoppingCart, faBars, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart, faBars, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt)
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+ 
     <App />
-  </React.StrictMode>,
+  
 
 );
 
